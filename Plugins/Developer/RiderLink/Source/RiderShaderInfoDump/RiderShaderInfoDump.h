25d0897a0d3d3078b68f4ba99894df9b65f24a8f
William DelGiudice <49406672+delgoodie@users.noreply.github.com>
2022-08-21 18:53:22 -0400

    Delete Plugins/Developer/RiderLink directory

--- Plugins/Developer/RiderLink/Source/RiderShaderInfoDump/RiderShaderInfoDump.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-#include "Modules/ModuleInterface.h"
-
-class RIDERSHADERINFODUMP_API FRiderShaderInfoDumpModule : public IModuleInterface
-{
-public:
-	virtual void StartupModule() override;
-};
